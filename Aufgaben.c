42151d868626d9709fbbfe0628e0a8c6703d73e9
Finja3 <Finja.rolke@web.de>
2025-08-27 14:27:53 +0200

    Tolle neue Aufgabe

--- Aufgaben.c
+++ Aufgaben.c
@@ -2,7 +2,14 @@
 
 int main() {
 
-    printf("Hello, World!");
+
+    int i;
+    printf("Bitte geben sie eine Zahl eien: ");
+
+    scanf("%d",&i);
+
+    printf("Das ist deine Zahl: %d", i);
+
 
     return 0;
 }
